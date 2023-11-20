83dded4d3c9016e2844f115014158e66d281d586
Jason Ray <jayray.net@gmail.com>
2023-11-20 09:56:00 -0500

    unit test refactoring

--- kessel/tests/test_fqa.py
+++ kessel/tests/test_fqa.py
@@ -3,8 +3,10 @@ import uuid
 import os
 import unittest
 from kessel.kessel import FileQueueAdapter
+from kessel.kessel import QueueAdapter
 from kessel.kessel import Message
 import time
+import inspect
 
 _kessel_root_directory = '/tmp/kessel/unit-test'
 _timestamp = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
@@ -12,30 +14,124 @@ _timestamp = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
 
 def get_unique_base_path(tag: str = None):
     path = _kessel_root_directory
+    path = os.path.join(path, _timestamp)
     if tag:
         path = os.path.join(path, tag)
+
     path = os.path.join(path, str(uuid.uuid4()))
     return path
 
 
+class TestFqaCompliance(unittest.TestCase):
+
+    def queue_adapter_factory(self) -> QueueAdapter:
+        return FileQueueAdapter(base_path=get_unique_base_path('fqa-compliance'))
+
+    def test_enqueue_dequeue_message(self):
+        qa = self.queue_adapter_factory()
+        m1 = Message(payload='hello world')
+        m1 = qa.enqueue(m1)
+
+        dq_1 = qa.dequeue_next()
+
+        self.assertEqual(dq_1.payload, 'hello world')
+        self.assertEqual(dq_1.id, m1.id)
+
+    def test_dequeue_skip_locked_message_with_1(self):
+        qa = self.queue_adapter_factory()
+        m1 = Message(payload='hello world')
+        m1 = qa.enqueue(m1)
+
+        dq_1 = qa.dequeue_next()
+        self.assertIsNotNone(dq_1)
+
+        dq_2 = qa.dequeue_next()
+        self.assertIsNone(dq_2)
+
+    def test_dequeue_skip_locked_message_with_2(self):
+        qa = self.queue_adapter_factory()
+        m1 = Message(payload='hello world m1')
+        print('enqueue m1')
+        m1 = qa.enqueue(m1)
+        print('enqueue complete', m1.id)
+
+        m2 = Message(payload='hello world m2')
+        print('enqueue m2')
+        m2 = qa.enqueue(m2)
+        print('enqueue complete', m2.id)
+
+        print('first dequeue, expect m1')
+        dq1 = qa.dequeue_next()
+        self.assertEqual(dq1.payload, m1.payload)
+        print('second dequeue, expect m2')
+        dq2 = qa.dequeue_next()
+        self.assertEqual(dq2.payload, m2.payload)
+
+        # because message is locked, should not be able to dequeue
+        dq3 = qa.dequeue_next()
+        self.assertIsNone(dq3)
+
+    def test_commit_removes_message(self):
+        qa = self.queue_adapter_factory()
+        m1 = Message(payload='hello world')
+        m1 = qa.enqueue(m1)
+
+        dq_1 = qa.dequeue_next()
+        self.assertIsNotNone(dq_1)
+
+        # at this point, m1 should exist and be locked
+
+        # because message is locked, it should not be available for dequeue
+        dq_2 = qa.dequeue_next()
+        self.assertIsNone(dq_2)
+
+        qa.commit(dq_1)
+
+        # at this point, m1 should not exist
+
+        dq_3 = qa.dequeue_next()
+        self.assertIsNone(dq_3)
+
+    def test_rollback_removes_lock(self):
+        qa = self.queue_adapter_factory()
+        m1 = Message(payload='hello world')
+        m1 = qa.enqueue(m1)
+
+        dq_1 = qa.dequeue_next()
+        self.assertIsNotNone(dq_1)
+
+        # at this point, m1 should exist and be locked
+
+        # because message is locked, it should not be available for dequeue
+        dq_2 = qa.dequeue_next()
+        self.assertIsNone(dq_2)
+
+        qa.rollback(dq_1)
+
+        # at this point, m1 should exist and be available for dequeue
+
+        dq_3 = qa.dequeue_next()
+        self.assertIsNotNone(dq_3)
+
+
 class TestFqa(unittest.TestCase):
-    _kessel_root_directory = '/tmp/kessel/unit-test'
-    _kessel_directory = os.path.join(_kessel_root_directory,
-                                     datetime.datetime.now().strftime("%Y%m%d-%H%M%S"))
+
+    def file_queue_adapter_factory(self) -> FileQueueAdapter:
+        return FileQueueAdapter(base_path=get_unique_base_path('fqa'))
 
     def test_construct(self):
-        fqa = FileQueueAdapter(base_path=get_unique_base_path())
+        fqa = self.file_queue_adapter_factory()
         self.assertIsNotNone(fqa)
 
     def test_publish_message(self):
-        fqa = FileQueueAdapter(base_path=get_unique_base_path())
+        fqa = self.file_queue_adapter_factory()
         m = Message(payload='hello world')
         m = fqa.enqueue(m)
 
         self.assertTrue(fqa._does_message_exist(m.id))
 
     def test_dequeue_message(self):
-        fqa = FileQueueAdapter(base_path=get_unique_base_path())
+        fqa = self.file_queue_adapter_factory()
         m1 = Message(payload='hello world')
         m1 = fqa.enqueue(m1)
 
@@ -47,43 +143,8 @@ class TestFqa(unittest.TestCase):
         self.assertTrue(fqa._does_message_exist(m1.id))
         self.assertTrue(fqa._does_lock_exist(m1.id))
 
-    def test_two_cannot_dequeue_same_record(self):
-        fqa = FileQueueAdapter(base_path=get_unique_base_path())
-        m1 = Message(payload='hello world')
-        m1 = fqa.enqueue(m1)
-
-        dq_1 = fqa.dequeue_next()
-        self.assertIsNotNone(dq_1)
-
-        # because message is locked, should not be able to dequeue
-        dq_2 = fqa.dequeue_next()
-        self.assertIsNone(dq_2)
-
-    def test_two_dequeue_nonlocked_record(self):
-        fqa = FileQueueAdapter(base_path=get_unique_base_path())
-        m1 = Message(payload='hello world m1')
-        print('enqueue m1')
-        m1 = fqa.enqueue(m1)
-        print('enqueue complete', m1.id)
-
-        m2 = Message(payload='hello world m2')
-        print('enqueue m2')
-        m2_path_file = fqa.enqueue(m2)
-        print('enqueue complete', m2.id)
-
-        print('first dequeue, expect m1')
-        dq1 = fqa.dequeue_next()
-        self.assertEqual(dq1.payload, m1.payload)
-        print('first dequeue, expect m2')
-        dq2 = fqa.dequeue_next()
-        self.assertEqual(dq2.payload, m2.payload)
-
-        # because message is locked, should not be able to dequeue
-        dq3 = fqa.dequeue_next()
-        self.assertIsNone(dq3)
-
     def test_commit_removes_message(self):
-        fqa = FileQueueAdapter(base_path=self.get_unique_base_path())
+        fqa = self.file_queue_adapter_factory()
         m1 = Message(payload='hello world')
         m1 = fqa.enqueue(m1)
 
@@ -103,7 +164,7 @@ class TestFqa(unittest.TestCase):
         self.assertFalse(fqa._does_lock_exist(m1.id))
 
     def test_rollback_removes_lock(self):
-        fqa = FileQueueAdapter(base_path=self.get_unique_base_path())
+        fqa = self.file_queue_adapter_factory()
         m1 = Message(payload='hello world')
         m1 = fqa.enqueue(m1)
 
@@ -127,20 +188,20 @@ class TestFqa(unittest.TestCase):
             contents = f.read()
         return contents
 
-    def get_unique_base_path(self, tag: str = None):
-        path = self._kessel_directory
-        print('get_unique_base_path p1', path)
-        if tag:
-            path = os.path.join(path, tag)
-        print('get_unique_base_path p2', path)
-        path = os.path.join(path, str(uuid.uuid4()))
-        print('get_unique_base_path p3', path)
-        return path
+    # def get_unique_base_path(self, tag: str = None):
+    #     path = self._kessel_directory
+    #     print('get_unique_base_path p1', path)
+    #     if tag:
+    #         path = os.path.join(path, tag)
+    #     print('get_unique_base_path p2', path)
+    #     path = os.path.join(path, str(uuid.uuid4()))
+    #     print('get_unique_base_path p3', path)
+    #     return path
 
     def test_enqueue_dequeue_x100(self):
         message_count = 100
         messages = []
-        fqa = FileQueueAdapter(base_path=self.get_unique_base_path())
+        fqa = self.file_queue_adapter_factory()
 
         for i in range(0, message_count):
             m = Message(payload=f'm{i}')
