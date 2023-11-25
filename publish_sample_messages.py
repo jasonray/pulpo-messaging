9e0944c0499eeeedab725045b1185f30ca474c28
Jason Ray <jayray.net@gmail.com>
2023-11-24 22:07:11 -0600

    config on publish message

--- publish_sample_messages.py
+++ publish_sample_messages.py
@@ -1,5 +1,6 @@
 from kessel.kessel import Message
 from kessel.kessel import Kessel
+from kessel.kessel import KesselConfig
 from statman import Statman
 import argparse
 
@@ -8,10 +9,21 @@ parser = argparse.ArgumentParser(
                     description='Published a set of test messages to kessel')
 
 parser.add_argument('--n', dest='number_of_messages', type=int, default=100)
+parser.add_argument('--config', type=str, help='path to config file')
 args = parser.parse_args()
 
+args = parser.parse_args()
 
-kessel = Kessel()
+config = None
+if args.config:
+    print('load config from file', args.config)
+    config = KesselConfig(json_file_path=args.config)
+else:
+    config = KesselConfig()
+
+config.process_args(args)
+
+kessel = Kessel(config)
 
 Statman.stopwatch(name='publish-sample-messages.timing', autostart=True)
 for i in range(0, args.number_of_messages):
