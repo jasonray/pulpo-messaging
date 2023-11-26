e330f64351a70a960296892a7b593aa15b0fe323
Jason Ray <jayray.net@gmail.com>
2023-11-25 21:22:14 -0500

    support 5 parallel runners

--- launch-kessel-runners.sh
+++ launch-kessel-runners.sh
@@ -1,8 +1,8 @@
 truncate -s 0 /tmp/kessel/kessel.log
 
 python3 kessel-runner.py --config=kessel-config.json >> /tmp/kessel/kessel.log 2>&1 &
-# python3 kessel-runner.py --config=kessel-config.json >> /tmp/kessel/kessel.log 2>&1 &
-# python3 kessel-runner.py --config=kessel-config.json >> /tmp/kessel/kessel.log 2>&1 &
-# python3 kessel-runner.py --config=kessel-config.json >> /tmp/kessel/kessel.log 2>&1 &
-# python3 kessel-runner.py --config=kessel-config.json >> /tmp/kessel/kessel.log 2>&1 &
+python3 kessel-runner.py --config=kessel-config.json >> /tmp/kessel/kessel.log 2>&1 &
+python3 kessel-runner.py --config=kessel-config.json >> /tmp/kessel/kessel.log 2>&1 &
+python3 kessel-runner.py --config=kessel-config.json >> /tmp/kessel/kessel.log 2>&1 &
+python3 kessel-runner.py --config=kessel-config.json >> /tmp/kessel/kessel.log 2>&1 &
 
