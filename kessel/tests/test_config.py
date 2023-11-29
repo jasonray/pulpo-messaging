8b023ec2abe7e2871492fab8e15bfbdb25e0bf94
Jason Ray <jayray.net@gmail.com>
2023-11-29 13:15:50 -0500

    reduce config tests

--- kessel/tests/test_config.py
+++ kessel/tests/test_config.py
@@ -10,45 +10,17 @@ class TestConfig(unittest.TestCase):
         config = Config(options=options)
         self.assertEqual(config.get('k'), 'v')
 
-    def test_config_get_empty(self):
-        options = {}
-        options['k'] = 'v'
-        config = Config(options=options)
-        self.assertEqual(config.get('k2'), None)
-
     def test_config_set(self):
         config = Config()
         config.set('k', 'v')
         # print('config', config.__options)
         self.assertEqual(config.get('k'), 'v')
 
-    def test_config_manual_nested(self):
-        options = {}
-        options['k'] = {'k2': 'v'}
-        config = Config(options)
-        self.assertEqual(config.get('k').get('k2'), 'v')
-
-    def test_config_set_nested(self):
-        config = Config()
-        config.set('k.k2', 'v')
-        self.assertEqual(config.get('k').get('k2'), 'v')
-
     def test_config_set_nested(self):
         config = Config()
         config.set('k.k2a.k3', 'v1')
-        config.set('k.k2b.k3a', 'v2')
-        config.set('k.k2b.k3b', 'v3')
         self.assertEqual(config.get('k').get('k2a').get('k3'), 'v1')
-        self.assertEqual(config.get('k').get('k2b').get('k3a'), 'v2')
-        self.assertEqual(config.get('k').get('k2b').get('k3b'), 'v3')
-
         self.assertEqual(config.get('k.k2a.k3'), 'v1')
-        self.assertEqual(config.get('k.k2b.k3a'), 'v2')
-        self.assertEqual(config.get('k.k2b.k3b'), 'v3')
-
-        self.assertIsNone(config.get('k.k2b.k3d'))
-        self.assertIsNone(config.get('k.k2b.k3.x'))
-        self.assertIsNone(config.get('k.k2b.x.x'))
 
     def test_load_config_from_file(self):
         config = Config(json_file_path='./kessel-config.json')
