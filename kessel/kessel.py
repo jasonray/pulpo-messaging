82d5ce81ee5cc641b545f7bb4c4aa827d5accce5
Jason Ray <jayray.net@gmail.com>
2023-12-04 20:55:30 -0500

    extract sample handler

--- kessel/kessel.py
+++ kessel/kessel.py
@@ -11,20 +11,6 @@ from pulpo_config import Config
 from art import text2art
 
 
-class PayloadHandler():
-    _config = None
-
-    def __init__(self, options: dict = None):
-        self._config = Config(options=options)
-
-    def handle(self, payload: str):
-        pass
-
-    @property
-    def config(self) -> Config:
-        return self._config
-
-
 class UpperCaseHandler(echo_handler):
 
     def handle(self, payload: str):
