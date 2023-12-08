22e78fae0645784aa31ea032eed5da2e3f253d0f
Jason Ray <jayray.net@gmail.com>
2023-12-07 21:15:21 -0500

    rename constructor param
    Signed-off-by: Jason Ray <jayray.net@gmail.com>

--- pulpo_messaging/tests/test_fqa.py
+++ pulpo_messaging/tests/test_fqa.py
@@ -154,7 +154,7 @@ class TestFqa(unittest.TestCase):
         payload += 'this statment has a "quote" - watch out \n'
         payload += "this statment has a 'single quote' \n"
         payload += "\t this starts with a tab \n"
-        m1 = Message(payload=payload, headers={'h1'})
+        m1 = Message(payload=payload, header={'h1'})
         m1 = qa.enqueue(m1)
         self.assertIsNotNone(m1.id)
 
@@ -169,7 +169,7 @@ class TestFqa(unittest.TestCase):
         qa = self.file_queue_adapter_factory()
         qa.config.set('skip_random_messages_range', 100)
 
-        m1 = Message(payload='test', headers={'h1'})
+        m1 = Message(payload='test', header={'h1'})
         m1 = qa.enqueue(m1)
 
         dq_1 = qa.dequeue()
