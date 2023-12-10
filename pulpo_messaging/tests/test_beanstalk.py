b1c2dacd378f056f3ad31fa98c1a15a04acc9f01
Jason Ray <jayray.net@gmail.com>
2023-12-10 11:14:08 -0500

    beanstalkd test setup
    Signed-off-by: Jason Ray <jayray.net@gmail.com>

--- pulpo_messaging/tests/test_beanstalk.py
+++ pulpo_messaging/tests/test_beanstalk.py
@@ -19,57 +19,68 @@ from contextlib import contextmanager
 from datetime import datetime, timedelta
 from typing import Callable, Iterable, Iterator, Optional, Union
 
-# BEANSTALKD_PATH = os.getenv("BEANSTALKD_PATH", "beanstalkd")
-# DEFAULT_INET_ADDRESS = ("127.0.0.1", 4444)
-# DEFAULT_UNIX_ADDRESS = "/tmp/greenstalk-test.sock"
+from greenstalk import (
+    DEFAULT_PRIORITY,
+    DEFAULT_TTR,
+    DEFAULT_TUBE,
+    Address,
+    BuriedError,
+    Client,
+    DeadlineSoonError,
+    DrainingError,
+    Job,
+    JobTooBigError,
+    NotFoundError,
+    NotIgnoredError,
+    TimedOutError,
+    UnknownResponseError,
+    _parse_chunk,
+    _parse_response,
+)
 
-# TestFunc = Callable[[Client], None]
-# WrapperFunc = Callable[[], None]
-# DecoratorFunc = Callable[[TestFunc], WrapperFunc]
+BEANSTALKD_PATH = os.getenv("BEANSTALKD_PATH", "beanstalkd")
+DEFAULT_INET_ADDRESS = ("127.0.0.1", 4444)
 
+TestFunc = Callable[[Client], None]
+WrapperFunc = Callable[[], None]
+DecoratorFunc = Callable[[TestFunc], WrapperFunc]
 
-# def with_beanstalkd(
-#     address: Address = DEFAULT_INET_ADDRESS,
-#     encoding: Optional[str] = "utf-8",
-#     use: str = DEFAULT_TUBE,
-#     watch: Union[str, Iterable[str]] = DEFAULT_TUBE,
-# ) -> DecoratorFunc:
-#     def decorator(test: TestFunc) -> WrapperFunc:
-#         def wrapper() -> None:
-#             cmd = [BEANSTALKD_PATH]
-#             if isinstance(address, str):
-#                 cmd.extend(["-l", "unix:" + address])
-#             else:
-#                 host, port = address
-#                 cmd.extend(["-l", host, "-p", str(port)])
-#             with subprocess.Popen(cmd) as beanstalkd:
-#                 time.sleep(0.1)
-#                 try:
-#                     with Client(address, encoding=encoding, use=use, watch=watch) as c:
-#                         test(c)
-#                 finally:
-#                     beanstalkd.terminate()
+def with_beanstalkd(
+    address: Address = DEFAULT_INET_ADDRESS,
+    encoding: Optional[str] = "utf-8",
+    tube: str = DEFAULT_TUBE,
+) -> DecoratorFunc:
+    print('with_beanstalkd')
+    print('define decorator')
+    def decorator(test: TestFunc) -> WrapperFunc:
+        def wrapper(cls) -> None:
+            cmd = [BEANSTALKD_PATH]
+            host, port = address
+            cmd.extend(["-l", host, "-p", str(port)])
+            print(f'starting beanstalkd [{cmd=}][{address=}]')
+            with subprocess.Popen(cmd) as beanstalkd:
+                print(f'started beanstalkd {beanstalkd.pid=}')
+                time.sleep(0.1)
+                try:
+                    options = {}
+                    options['host'] = host
+                    options['port'] = port
+                    options['encoding'] = encoding
+                    options['default_tube'] = tube
+                    bqa= BeanstalkdQueueAdapter(options=options)
+                    test(bqa)
+                finally:
+                    print(f'terminating beanstalkd {beanstalkd.pid=}')
+                    beanstalkd.terminate()
+        return wrapper
+    return decorator
 
-#         return wrapper
+class TestBeanstalkQueueAdapterCompliance():
 
-#     return decorator
+    @with_beanstalkd()
+    def test_enqueue_dequeue_message(qa: QueueAdapter):
+        print(f'test_enqueue_dequeue_message {qa=}')
 
-
-class TestBeanstalkQueueAdapterCompliance(unittest.TestCase):
-    # To start beanstalkd now and restart at login:
-    #   brew services start beanstalkd
-    # Or, if you don't want/need a background service you can just run:
-    #   /opt/homebrew/opt/beanstalkd/bin/beanstalkd -l 127.0.0.1 -p 11300
-
-    def queue_adapter_factory(self) -> QueueAdapter:
-        options = {}
-        options['host'] = '127.0.0.1'
-        options['port'] = 11300
-        options['socket_timeout'] = 10
-        return BeanstalkdQueueAdapter(options=options)
-
-    def test_enqueue_dequeue_message(self):
-        qa = self.queue_adapter_factory()
         m1 = Message(payload='hello world')
         print('invoke enqueue')
         m1 = qa.enqueue(m1)
@@ -79,21 +90,22 @@ class TestBeanstalkQueueAdapterCompliance(unittest.TestCase):
         dq_1 = qa.dequeue()
         print(f'dequeue complete {dq_1=}')
 
-        self.assertEqual(dq_1.id, m1.id)
-        self.assertEqual(dq_1.payload, 'hello world')
+        assert dq_1.id == m1.id
+        assert dq_1.payload == 'hello world'
 
-    # def test_enqueue_dequeue_with_body(self):
-    #     qa = self.queue_adapter_factory()
-    #     m1 = Message(body={'k': 'v'}, payload='hello world')
-    #     m1 = qa.enqueue(m1)
+    @with_beanstalkd()
+    def test_enqueue_dequeue_with_body(qa: QueueAdapter):
+        m1 = Message(body={'k': 'v'}, payload='hello world')
+        m1 = qa.enqueue(m1)
 
-    #     dq_1 = qa.dequeue()
+        dq_1 = qa.dequeue()
 
-    #     self.assertEqual(dq_1.payload, 'hello world')
-    #     self.assertEqual(dq_1.get_body_item('k'), 'v')
-    #     self.assertEqual(dq_1.id, m1.id)
+        assert dq_1.payload == 'hello world'
+        assert dq_1.get_body_item('k') == 'v'
+        assert dq_1.id == m1.id
 
-    # def test_enqueue_dequeue_with_header(self):
+    # @with_beanstalkd()
+    # def test_enqueue_dequeue_with_header(qa: QueueAdapter):
     #     qa = self.queue_adapter_factory()
     #     m1 = Message(header={'k': 'v'})
     #     m1 = qa.enqueue(m1)
