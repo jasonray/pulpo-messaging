9e6f0c1d41236e72d5884446d2d63321b599001b
Jason Ray <jayray.net@gmail.com>
2023-12-16 16:13:51 -0500

    remove debugging
    Signed-off-by: Jason Ray <jayray.net@gmail.com>

--- pulpo_messaging/message.py
+++ pulpo_messaging/message.py
@@ -40,7 +40,6 @@ class Message():
                 self.set_header_item(key=key, value=header[key])
         elif isinstance(header, set):
             for key in header:
-                # print(f'processing headers: {key=}')
                 self.set_header_item(key=key, value=None)
 
     def __store_body(self, body):
@@ -48,7 +47,6 @@ class Message():
             self.set_body_item(key=key, value=body[key])
 
     def get(self, key: str):
-        # print(f'message.get {key=}')
         keys = key.split('.')
 
         value = self._components
@@ -61,7 +59,6 @@ class Message():
             else:
                 value = None
 
-        # print(f'message.get {key=} => {value=}')
         return value
 
     def getAsDate(self, key: str):
@@ -71,7 +68,6 @@ class Message():
         return value
 
     def set(self, key: str, value: typing.Any):
-        # print(f'message.set {key=} {value=}')
         keys = key.split('.')
 
         parent = self._components
@@ -112,10 +108,8 @@ class Message():
     @delay.setter
     def delay(self, value):
         if isinstance(value, timedelta):
-            # print('setting delay as timedelta')
             delta_dt = datetime.datetime.now() + value
         else:
-            # print('setting delay as date')
             delta_dt = value
         fqk = 'header.delay'
         self.set(fqk, delta_dt)
@@ -144,7 +138,6 @@ class Message():
 
     @property
     def attempts(self) -> int:
-        # print('get attempts')
         header_item = self.get_header_item('attempts')
         if header_item is None:
             value = 0
