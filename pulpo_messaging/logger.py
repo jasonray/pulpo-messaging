82f46088216047686563ae33eae793aae44d011b
Jason Ray <jayray.net@gmail.com>
2023-12-19 19:10:08 -0500

    pass through to logger

--- pulpo_messaging/logger.py
+++ pulpo_messaging/logger.py
@@ -1,17 +1,7 @@
 import datetime
 import os
+from loguru import logger
 
 
 def log(*values: object, flush: bool = False):
-    message = ""
-    for arg in values:
-        if not arg is None:
-            message += ' ' + str(arg)
-
-    pid = os.getpid()
-    dt = datetime.datetime.now().strftime("%m/%d/%Y %H:%M:%S.%f")
-
-    TEMPLATE = "[p:{pid}]\t[{dt}]\t{message}"
-    output = TEMPLATE.format(pid=pid, dt=dt, message=message)
-
-    print(output, flush=flush)
+    logger.info(*values)
