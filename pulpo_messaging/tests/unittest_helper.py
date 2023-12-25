408e598761c548604c97f17d86d36811711e9e28
Jason Ray <jayray.net@gmail.com>
2023-12-25 16:43:21 -0500

    rename directory
    Signed-off-by: Jason Ray <jayray.net@gmail.com>

--- pulpo_messaging/tests/unittest_helper.py
+++ pulpo_messaging/tests/unittest_helper.py
@@ -2,12 +2,12 @@ import datetime
 import uuid
 import os
 
-_kessel_root_directory = '/tmp/pulpo/unit-test'
+_pulpo_root_directory = '/tmp/pulpo/unit-test'
 _timestamp = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
 
 
 def get_unique_base_path(tag: str = None):
-    path = _kessel_root_directory
+    path = _pulpo_root_directory
     path = os.path.join(path, _timestamp)
     if tag:
         path = os.path.join(path, tag)
